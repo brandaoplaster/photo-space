94b7e13c4c0b4dc9358eb7fb397f6bfe61efcfb7
brandaoplaster <brandaoplaster@gmail.com>
2025-11-12 20:14:50 -0300

    feat: Add image and button delete in page photo details

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,6 +1,8 @@
 import Container from "../components/container";
 import Text from "../components/text";
 import Skeleton from "../components/skeleton";
+import ImagePreview from "../components/image-preview";
+import Button from "../components/button";
 import type { Photo } from "../context/photos/models/photo";
 import PhotosNavigator from "../context/photos/components/photos-navigator";
 
@@ -28,6 +30,26 @@ export default function PagePhotoDetails() {
 
         <PhotosNavigator />
       </header>
+
+      <div className="grid grid-cols-[21rem] gap-24">
+        <div className="space-y-3">
+          {!isLoadingPhoto ? (
+            <ImagePreview
+              src={`/images/${photo?.imageId}`}
+              title={photo.title}
+              imageClassName="h-[21rem]"
+            />
+          ) : (
+            <Skeleton className="h-[21rem]" />
+          )}
+
+          {!isLoadingPhoto ? (
+            <Button variant="destructive">Excluir</Button>
+          ) : (
+            <Skeleton className="w-20 h-10" />
+          )}
+        </div>
+      </div>
     </Container>
   );
 }
