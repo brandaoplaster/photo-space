f71bf39c0a14b597f72412f687dbce394ed376e5
brandaoplaster <brandaoplaster@gmail.com>
2025-11-12 20:25:23 -0300

    feat: Adjust main header component to use AlbumNewDialog component

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -6,6 +6,7 @@ import Button from "./button";
 import PhotosSearch from "./photos-search";
 import Divider from "./divider";
 import PhotoNewDialog from "../context/photos/components/photo-new-dialog";
+import AlbumNewDialog from "../context/albums/models/components/album-new-dialog";
 
 interface MainHeaderProps extends React.ComponentProps<typeof Container> {}
 
@@ -25,9 +26,10 @@ export default function MainHeader({ className, ...props }: MainHeaderProps) {
       <Divider orientation="vertical" className="h-10" />
 
       <div className="flex items-center gap-3">
-        <PhotoNewDialog trigger={<Button>New Photo</Button>} />
-
-        <Button variant="secondary">Criar álbum</Button>
+        <PhotoNewDialog trigger={<Button>Nova foto</Button>} />
+        <AlbumNewDialog
+          trigger={<Button variant="secondary">Criar álbum</Button>}
+        />
       </div>
     </Container>
   );
